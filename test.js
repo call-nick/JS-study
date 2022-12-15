fc1edb656ffb10014712ae0e707d68404d8a9b73
call-nick <120306885+call-nick@users.noreply.github.com>
2022-12-15 09:11:14 +0300

    Update test.js

--- test.js
+++ test.js
@@ -2,6 +2,15 @@ describe("pow", function() {
 
   function makeTest(x) {
     let expected = x * x * x;
+    
+      it("для отрицательных n возвращает NaN", function() {
+    assert.isNaN(pow(2, -1));
+  });
+
+  it("для дробных n возвращает NaN", function() {
+    assert.isNaN(pow(2, 1.5));
+  });
+    
     it(`${x} в степени 3 будет ${expected}`, function() {
       assert.equal(pow(x, 3), expected);
     });
